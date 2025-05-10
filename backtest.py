d8145d8dc4da83bacc95bf7db0060a5b04dcd89e
z14r4 <444ziara@gmail.com>
2025-05-10 02:59:09 -0700

    lastt requirements fr
    :D

--- backtest.py
+++ backtest.py
@@ -7,6 +7,7 @@ from math import sqrt
 import statsmodels.api as sm
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 from sklearn.model_selection import cross_val_score
+# Import the library that contains all the functions/modules related to the regression model
 
 
 X = pd.read_csv(r"X.csv")
@@ -17,7 +18,6 @@ X['egg_price'] = egg
 # Add a new column 'Y' which is the 'egg_price' column shifted down by 1 entry
 X['Y'] = X['egg_price'].shift(-1)
 X_train, X_test, y_train, y_test = train_test_split(X.drop(columns=['Y', 'Year-Month']), X['Y'], test_size=0.25, shuffle = False)
-# Import the library that contains all the functions/modules related to the regression model
 
 # We must add an intercept as the standard model doesn't automatically fit one
 X_train = sm.add_constant(X_train)
@@ -25,11 +25,10 @@ X_test = sm.add_constant(X_test)
 lr = LinearRegression()
 lr.fit(X_train, y_train)
 pred = lr.predict(X_test)
+
 # Calculate the mean squared error and R-squared value  
 mse = mean_squared_error(y_test.iloc[:-1], pred[:-1])
 r2 = r2_score(y_test.iloc[:-1], pred[:-1])
-print(f'Root Mean Squared Error: {sqrt(mse)}')
-print(f'R-squared: {r2}')
 
 # Calculate the residuals
 residuals = y_test.iloc[:-1] - pred[:-1]
